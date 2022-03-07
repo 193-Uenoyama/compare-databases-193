16e61506fc55b06f91c36c3ce764d4999cae3ee6
193-Uenoyama <dokkoikitamura193@gmail.com>
2022-03-07 21:23:23 +0900

    install tsc

--- src/request-page/webpack.config.js
+++ src/request-page/webpack.config.js
@@ -7,6 +7,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(ts|tsx)$/,
+        use: "ts-loader",
+      },
+      {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules)/,
         loader: "babel-loader",
