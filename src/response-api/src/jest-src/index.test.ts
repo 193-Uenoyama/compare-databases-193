8da337fa3e6af8e074905e984de4855727c00379
193-Uenoyama <dokkoikitamura193@gmail.com>
2022-03-26 16:13:43 +0900

    update serv test name

--- src/response-api/src/jest-src/index.test.ts
+++ src/response-api/src/jest-src/index.test.ts
@@ -1,26 +1,28 @@
 import appTest from '@/jest-src/express-routes/app.test';
-import userTest from '@/jest-src/express-routes/user.test';
-import groupTest from '@/jest-src/express-routes/group.test';
-import belongsToGroupTest from '@/jest-src/express-routes/groupmember.test';
-import followTest from '@/jest-src/express-routes/follow.test';
 
-import writeLogTest from '@/jest-src/express-logs/writelogs.test';
-import userLogTest from '@/jest-src/express-logs/userlogs.test';
-import groupLogTest from '@/jest-src/express-logs/grouplogs.test';
-import groupMemberLogTest from '@/jest-src/express-logs/groupmemberlogs.test';
-import followLogTest from '@/jest-src/express-logs/followlogs.test';
+import userRouter from '@/jest-src/express-routes/userRouter.test';
+import groupRouter from '@/jest-src/express-routes/groupRouter.test';
+import belongsToGroupRouter from '@/jest-src/express-routes/groupMemberRouter.test';
+import followRouter from '@/jest-src/express-routes/followRouter.test';
+
+import writeLogs from '@/jest-src/express-logs/writeLogs.test';
+import userLogs from '@/jest-src/express-logs/userLogs.test';
+import groupLogs from '@/jest-src/express-logs/groupLogs.test';
+import belongsToGroupLogs from '@/jest-src/express-logs/belongsToGroupLogs.test';
+import followLogs from '@/jest-src/express-logs/followLogs.test';
 
 // 同期的にテストを実行する
 describe("compare-databases-serv-test", () => {
   appTest
-  userTest
-  groupTest
-  belongsToGroupTest
-  followTest
 
-  writeLogTest
-  userLogTest
-  groupLogTest
-  groupMemberLogTest
-  followLogTest
+  userRouter
+  groupRouter
+  belongsToGroupRouter
+  followRouter 
+                      
+  writeLogs 
+  userLogs 
+  groupLogs 
+  belongsToGroupLogs 
+  followLogs 
 });
